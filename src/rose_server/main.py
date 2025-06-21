5b290fbdb418ff3a4228e8d5c3eaefbd810ec367
shaydevelops <31566740+shaydevelops@users.noreply.github.com>
2025-06-21 11:16:59 -0400

    removed signal handler

--- src/rose_server/main.py
+++ src/rose_server/main.py
@@ -1,19 +1,10 @@
-import signal
-import sys
-
 import uvicorn
 
 from rose_core.config.service import HOST, PORT
 
 
-def handle_exit(signum, frame):
-    print("\nReceived signal to terminate. Shutting down...")
-    sys.exit(0)
-
-
 def main():
-    signal.signal(signal.SIGINT, handle_exit)
-    signal.signal(signal.SIGTERM, handle_exit)
+    # Let uvicorn handle signals properly
     uvicorn.run(
         "rose_server.app:app",
         host=HOST,
