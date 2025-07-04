20c961bf8f4b194e8c3ea83663337467c645d029
shaydevelops <31566740+shaydevelops@users.noreply.github.com>
2025-07-04 01:59:48 -0400

    model management

--- src/rose_cli/models/__init__.py
+++ src/rose_cli/models/__init__.py
@@ -4,6 +4,7 @@ from rose_cli.models.add import add_model
 from rose_cli.models.get import get_model
 from rose_cli.models.list import list_models
 from rose_cli.models.pull import pull_model
+from rose_cli.models.seed import seed_models
 
 app = typer.Typer()
 
@@ -11,3 +12,4 @@ app.command(name="list")(list_models)
 app.command(name="get")(get_model)
 app.command(name="pull")(pull_model)
 app.command(name="add")(add_model)
+app.command(name="seed")(seed_models)
