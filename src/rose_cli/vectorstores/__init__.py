1cbbc709530ad4fd3e6ea46006392713da3b453f
shaydevelops <31566740+shaydevelops@users.noreply.github.com>
2025-08-09 11:25:05 -0400

    vectorstores create command

--- src/rose_cli/vectorstores/__init__.py
+++ src/rose_cli/vectorstores/__init__.py
@@ -1,7 +1,9 @@
 import typer
 
+from rose_cli.vectorstores.create import create_vectorstore
 from rose_cli.vectorstores.list import list_vectorstores
 
 app = typer.Typer()
 
 app.command(name="list")(list_vectorstores)
+app.command(name="create")(create_vectorstore)
