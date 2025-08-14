971af2349b1f8bd3e0625bbbc5aa4de2c4b18064
shaydevelops <31566740+shaydevelops@users.noreply.github.com>
2025-08-13 22:54:36 -0400

    Revert "dbmate --no-dump-schema"
    This reverts commit b044780bbad12e4f053ee5f914e8be1b1b31a414.

--- start.sh
+++ start.sh
@@ -30,7 +30,7 @@ export DBMATE_MIGRATIONS_DIR=${DBMATE_MIGRATIONS_DIR:-db/migrations}
 export DBMATE_SCHEMA_FILE=${DBMATE_SCHEMA_FILE:-db/schema.sql}
 
 # Apply migrations
-dbmate --no-dump-schema up
+dbmate up
 echo "Database migrations complete"
 
 # Function to cleanup on exit
