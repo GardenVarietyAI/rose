dc622cbaac62f0a1b936fdc5f685acde7340826f
shaydevelops <31566740+shaydevelops@users.noreply.github.com>
2025-08-14 20:46:17 -0400

    use file size when available

--- src/rose_server/files/router.py
+++ src/rose_server/files/router.py
@@ -22,7 +22,7 @@ async def create(
     """Upload a file."""
     try:
         content = await file.read()
-        file_size = len(content)
+        file_size = file.size or len(content)
 
         uploaded_file = await create_file(file_size=file_size, purpose=purpose, filename=file.filename, content=content)
 
