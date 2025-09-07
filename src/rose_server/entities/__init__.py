6b00e03b5bc3718354b51b23a86449b1af987124
shaydevelops <31566740+shaydevelops@users.noreply.github.com>
2025-09-07 19:28:43 -0400

    Remove unused database entity models

--- src/rose_server/entities/__init__.py
+++ src/rose_server/entities/__init__.py
@@ -3,28 +3,16 @@ This module contains all SQLModel models used for database tables.
 API schemas are in the schemas/ module.
 """
 
-from rose_server.entities.assistants import Assistant
 from rose_server.entities.fine_tuning import (
     FineTuningEvent,
     FineTuningJob,
 )
 from rose_server.entities.messages import Message
 from rose_server.entities.models import LanguageModel
-from rose_server.entities.run_steps import RunStep
-from rose_server.entities.runs import Run
-from rose_server.entities.threads import (
-    MessageMetadata,
-    Thread,
-)
 
 __all__ = [
     "FineTuningJob",
     "FineTuningEvent",
-    "Assistant",
-    "Thread",
     "Message",
-    "MessageMetadata",
-    "Run",
-    "RunStep",
     "LanguageModel",
 ]
