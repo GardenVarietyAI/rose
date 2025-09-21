8c9d866aa5d5bf3464d09abdad555178f9bf35c5
shaydevelops <31566740+shaydevelops@users.noreply.github.com>
2025-09-21 17:24:31 -0400

    Remove all custom database migration scripts

--- db/migrations/20250812000001_add_file_content_blob.sql
+++
@@ -1,7 +0,0 @@
--- migrate:up
--- Add BLOB storage support to files table
-ALTER TABLE files ADD COLUMN content BLOB;
-
--- migrate:down
--- Remove BLOB column (note: this will lose BLOB data)
-ALTER TABLE files DROP COLUMN content;
--- db/migrations/20250907000002_add_model_kind.sql
+++
@@ -1,5 +0,0 @@
--- migrate:up
-ALTER TABLE models ADD COLUMN kind TEXT;
-
--- migrate:down
-ALTER TABLE models DROP COLUMN kind;
