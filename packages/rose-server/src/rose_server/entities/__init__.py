7bb06e4a8d0877e867f558b440fce25d73509ffd
shaydevelops <31566740+shaydevelops@users.noreply.github.com>
2025-09-24 00:54:07 -0400

    Add file and vector store entities to __init__.py

--- packages/rose-server/src/rose_server/entities/__init__.py
+++ packages/rose-server/src/rose_server/entities/__init__.py
@@ -3,16 +3,24 @@ This module contains all SQLModel models used for database tables.
 API schemas are in the schemas/ module.
 """
 
+from rose_server.entities.file_chunks import FileChunk
+from rose_server.entities.files import UploadedFile
 from rose_server.entities.fine_tuning import (
     FineTuningEvent,
     FineTuningJob,
 )
 from rose_server.entities.messages import Message
 from rose_server.entities.models import LanguageModel
+from rose_server.entities.vector_stores import Document, VectorStore, VectorStoreFile
 
 __all__ = [
+    "FileChunk",
+    "UploadedFile",
     "FineTuningJob",
     "FineTuningEvent",
     "Message",
     "LanguageModel",
+    "Document",
+    "VectorStore",
+    "VectorStoreFile",
 ]
