1e2dee89167e6d55733875a8884babaec943f2a9
shaydevelops <31566740+shaydevelops@users.noreply.github.com>
2025-11-08 20:58:42 -0500

    removed v1/chat/completions

--- packages/rose-server/src/rose_server/router.py
+++ packages/rose-server/src/rose_server/router.py
@@ -2,7 +2,6 @@ import logging
 
 from fastapi import APIRouter
 
-from rose_server.routers.chat_completions import router as chat_completions_router
 from rose_server.routers.embeddings import router as embeddings_router
 from rose_server.routers.files import router as files_router
 from rose_server.routers.models import router as llms_router
@@ -20,5 +19,4 @@ router.include_router(embeddings_router)
 router.include_router(llms_router)
 router.include_router(responses_router)
 router.include_router(files_router)
-router.include_router(chat_completions_router)
 router.include_router(reranker_router)
