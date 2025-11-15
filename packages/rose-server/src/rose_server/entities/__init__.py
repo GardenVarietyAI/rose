9a959307a22701d5f915320fe405e8483032a427
shaydevelops <31566740+shaydevelops@users.noreply.github.com>
2025-11-15 00:04:21 -0500

    Remove vector store feature from CLI and server

--- packages/rose-server/src/rose_server/entities/__init__.py
+++ packages/rose-server/src/rose_server/entities/__init__.py
@@ -6,13 +6,9 @@ API schemas are in the schemas/ module.
 from rose_server.entities.files import UploadedFile
 from rose_server.entities.messages import Message
 from rose_server.entities.models import LanguageModel
-from rose_server.entities.vector_stores import Document, VectorStore, VectorStoreFile
 
 __all__ = [
     "UploadedFile",
     "Message",
     "LanguageModel",
-    "Document",
-    "VectorStore",
-    "VectorStoreFile",
 ]
--- packages/rose-server/src/rose_server/router.py
+++ packages/rose-server/src/rose_server/router.py
@@ -7,14 +7,10 @@ from rose_server.routers.files import router as files_router
 from rose_server.routers.models import router as llms_router
 from rose_server.routers.reranker import router as reranker_router
 from rose_server.routers.responses import router as responses_router
-from rose_server.routers.vector_store_files import router as vector_store_files_router
-from rose_server.routers.vector_stores import router as vector_stores_router
 
 logger = logging.getLogger(__name__)
 
 router = APIRouter()
-router.include_router(vector_stores_router)
-router.include_router(vector_store_files_router)
 router.include_router(embeddings_router)
 router.include_router(llms_router)
 router.include_router(responses_router)
