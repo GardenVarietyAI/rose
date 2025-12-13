65ccd330d67b340a8032036347f25a7f2a84d66b
shaydevelops <31566740+shaydevelops@users.noreply.github.com>
2025-12-13 12:50:52 -0500

    Remove reranker router and its registration

--- packages/rose-server/src/rose_server/router.py
+++ packages/rose-server/src/rose_server/router.py
@@ -2,9 +2,7 @@ from fastapi import APIRouter
 
 from rose_server.routers.chat import router as chat_router
 from rose_server.routers.models import router as models_router
-from rose_server.routers.reranker import router as reranker_router
 
 router = APIRouter()
 router.include_router(chat_router)
 router.include_router(models_router)
-router.include_router(reranker_router)
