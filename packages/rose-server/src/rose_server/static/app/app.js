2a259939377383c1c81a3bd09f8f12dc3f602e57
shaydevelops <31566740+shaydevelops@users.noreply.github.com>
2025-12-21 16:59:03 -0500

    expanding input

--- packages/rose-server/src/rose_server/static/app/app.js
+++ packages/rose-server/src/rose_server/static/app/app.js
@@ -1,9 +1,11 @@
 import { askButton } from "./components/ask-button.js";
 import { responseMessage } from "./components/response-message.js";
+import { searchForm } from "./components/search-form.js";
 import { threadPage } from "./pages/thread.js";
 
 document.addEventListener("alpine:init", () => {
   Alpine.data("askButton", askButton);
   Alpine.data("responseMessage", responseMessage);
+  Alpine.data("searchForm", searchForm);
   Alpine.data("threadPage", threadPage);
 });
