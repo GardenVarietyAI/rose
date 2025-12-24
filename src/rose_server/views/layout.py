dcfcf8a81001f99731576f266b9b8ad75b2de2b5
shaydevelops <31566740+shaydevelops@users.noreply.github.com>
2025-12-24 05:06:40 -0500

    foundation

--- src/rose_server/views/layout.py
+++ src/rose_server/views/layout.py
@@ -1,4 +1,4 @@
-from htpy import Node, a, body, h1, head, html, link, main, meta, script, title
+from htpy import Node, a, body, h1, head, html, link, main, meta, nav, script, style, title
 
 
 def render_page(*, title_text: str, content: Node) -> Node:
@@ -8,12 +8,17 @@ def render_page(*, title_text: str, content: Node) -> Node:
             title[title_text],
             meta(name="viewport", content="width=device-width, initial-scale=1"),
             meta(name="color-scheme", content="light dark"),
-            link(rel="stylesheet", href="/static/vendor/open-props/normalize.min.css"),
+            style(rel="text/css")["[x-cloak] { display: none !important }"],
             link(rel="stylesheet", href="/static/vendor/open-props/open-props.css"),
             link(rel="stylesheet", href="/static/app/app.css"),
         ],
         body[
             h1[a(href="/v1/search")["ROSE"]],
+            nav(class_="navbar")[
+                a(href="/v1/search")["Search"],
+                # a(href="/v1/threads")["Threads"],
+                a(href="/v1/lenses")["Lenses"],
+            ],
             main(class_="container")[content],
             script(src="/static/app/bundle.js", defer=True),
             script(src="/static/vendor/alpine/alpine.min.js", defer=True),
