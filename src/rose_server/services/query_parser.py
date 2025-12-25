f281fe18901da014ddaf45163bec274eb9e7f584
shaydevelops <31566740+shaydevelops@users.noreply.github.com>
2025-12-25 02:18:02 -0500

    at mention extraction

---
+++ src/rose_server/services/query_parser.py
@@ -0,0 +1,11 @@
+import re
+
+
+def parse_query(text: str) -> tuple[str, str, list[str]]:
+    """Extract @mentions from query."""
+    pattern = r"(?<!\w)@(\w+)"
+    lens_at_names = re.findall(pattern, text)
+    clean_query = re.sub(pattern, "", text)
+    clean_query = " ".join(clean_query.split())
+
+    return text, clean_query, lens_at_names
